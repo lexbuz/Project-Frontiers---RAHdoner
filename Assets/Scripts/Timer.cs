d18db002e6aa1caf24ece020ce6fdc08372dfc04
lexbuz <buzila.alexandra06@gmail.com>
2026-01-13 17:15:23 +0100

    Timer script
    - Created  timer C# script (can be implemented for stuff to happen after countdown)
    - Created a text mesh pro timer object with the script attached

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    float currentTime;
+    public float startingTime = 10f;
+    public TextMeshProUGUI countdownText;
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+    void Update()
+    {
+        if (currentTime > 0)
+        {
+            currentTime -= Time.deltaTime;
+        }
+        else if (currentTime < 0)
+        {
+            currentTime = 0;
+            // Timer has finished, you can add additional logic here if needed
+        }
+
+        int minutes = Mathf.FloorToInt(currentTime / 60);
+        int seconds = Mathf.FloorToInt(currentTime % 60);
+        countdownText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
