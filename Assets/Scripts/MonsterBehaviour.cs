ac2da02ed085eef047e202c332131f637b6f2070
lexbuz <buzila.alexandra06@gmail.com>
2026-01-19 02:01:15 +0100

    Implemented teleport
    - changed the teleport function
    - implemented a coroutine that is being called at random times between two constants
    - need to add an attack (instant KO) function

--- Assets/Scripts/MonsterBehaviour.cs
+++ Assets/Scripts/MonsterBehaviour.cs
@@ -1,12 +1,16 @@
+using System.Collections;
+using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class MonsterBehaviour : MonoBehaviour
 {
-    [Header ("References")]
     [SerializeField] private Transform[] patrolPoints;
+    [SerializeField] private Transform[] teleportPoints;
     private NavMeshAgent agent;
     private int currentPatrolIndex = 0;
+    private int currentTeleportIndex = 0;
+    private bool isTeleporting = false;
     void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
@@ -18,6 +22,11 @@ public class MonsterBehaviour : MonoBehaviour
     void Update()
     {
         Patrol();
+        if (!isTeleporting) 
+        {
+        isTeleporting = true;
+        StartCoroutine(Teleport());
+        }
     }
     private void Patrol()
     {
@@ -32,13 +41,13 @@ public class MonsterBehaviour : MonoBehaviour
         agent.SetDestination(patrolPoints[currentPatrolIndex].position);
         currentPatrolIndex = (currentPatrolIndex + 1) % patrolPoints.Length;
     }
-    private void Teleport()
+    IEnumerator Teleport()
     {
-        Vector3 randomPosition = new Vector3(
-            Random.Range(-50f, 50f),
-            0f,
-            Random.Range(-50f, 50f)
-        );
+        currentTeleportIndex = Random.Range(0, teleportPoints.Length);
+        Vector3 randomPosition = teleportPoints[currentTeleportIndex].position;
         agent.Warp(randomPosition);
+        int waitTime = Random.Range(5, 20);
+        yield return new WaitForSecondsRealtime(waitTime);
+        isTeleporting = false;
     }
 }
