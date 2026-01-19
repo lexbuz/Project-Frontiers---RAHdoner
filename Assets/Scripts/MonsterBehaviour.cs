316526ea2124e8d51c0a3ae741501ade94338d87
lexbuz <buzila.alexandra06@gmail.com>
2026-01-19 12:38:55 +0100

    Monster behaviour - loop 3
    - finished the script - added a jumpscare coroutine
    - when in range the monster will turn to face the player and then warp in front of it as a jumpscare, followed by a death screen
    
    WHAT DOES IT DO:
    - the monster patrols a certain path, determined by waypoints in unity
    - teleports at random times (between 2 constants) and continues patroling
    - when player gets in range the monster will turn to face the player and then jumpscare and kill

--- Assets/Scripts/MonsterBehaviour.cs
+++ Assets/Scripts/MonsterBehaviour.cs
@@ -7,6 +7,8 @@ public class MonsterBehaviour : MonoBehaviour
 {
     [SerializeField] private Transform[] patrolPoints;
     [SerializeField] private Transform[] teleportPoints;
+    [SerializeField] private Transform player;
+    [SerializeField] private GameObject deathScreen;
     private NavMeshAgent agent;
     private int currentPatrolIndex = 0;
     private int currentTeleportIndex = 0;
@@ -26,6 +28,15 @@ public class MonsterBehaviour : MonoBehaviour
         {
         isTeleporting = true;
         StartCoroutine(Teleport());
+        }
+        if (player != null)
+        {
+            bool inRange = Vector3.Distance(transform.position, player.position) < 1.2f;
+            if (inRange)
+            {
+                StartCoroutine(LookAtPlayer());
+                StartCoroutine(JumpScare());
+            }
         }
     }
     private void Patrol()
@@ -49,5 +60,21 @@ public class MonsterBehaviour : MonoBehaviour
         int waitTime = Random.Range(5, 20);
         yield return new WaitForSecondsRealtime(waitTime);
         isTeleporting = false;
+    }
+    IEnumerator LookAtPlayer()
+    {
+        Vector3 lookPosition = player.position - transform.position;
+        lookPosition.y = 0;
+        Quaternion rotation = Quaternion.LookRotation(lookPosition);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 0.2f);
+        yield return new WaitForSecondsRealtime(2);
+    }
+    IEnumerator JumpScare()
+    {
+        Vector3 jumpScarePosition = player.position + player.forward * 0.5f;
+        agent.Warp(jumpScarePosition);
+        yield return new WaitForSecondsRealtime(1);
+        deathScreen.SetActive(true);
+        yield return null;
     }
 }
